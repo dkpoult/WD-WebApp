b10d16a1a44329d93375adb994b483742774c541
Dylan Hanger <dylan.hanger@gmail.com>
2019-02-25 14:49:55 +0200

    Worked on Navbar
    Made navbar responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavComponent } from './nav/nav.component';
 
 const routes: Routes = [
 ];
