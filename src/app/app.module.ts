6c4eeaec9e78a3bfa94058b5f8ca48b66a84a2f3
Dylan Hanger <dylan.hanger@gmail.com>
2019-04-02 10:06:38 +0200

    Moved Material to a separate module
    Makes for cleaner code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatSidenavModule,
-  MatIconModule,
-  MatListModule,
-  MatDialogModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatTooltipModule
-} from '@angular/material';
 import { NavComponent } from './nav/nav.component';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { SignupDialogComponent } from './signup-dialog/signup-dialog.component';
@@ -28,6 +17,7 @@ import { CoursesComponent } from './courses/courses.component';
 import { MainComponent } from './main/main.component';
 import { CreateCourseComponent } from './create-course/create-course.component';
 import { LinkCourseComponent } from './link-course/link-course.component';
+import { MaterialModule } from './material/material.module';
 
 export function initialiseApp(config: SharedService) {
   return () => config.initialise();
@@ -54,15 +44,7 @@ export function initialiseApp(config: SharedService) {
     LayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTooltipModule,
+    MaterialModule
   ],
   providers: [
     {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 
-import { MatDialogRef, MatDialogModule } from '@angular/material';
+import {
+  MatDialogModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTooltipModule
+} from '@angular/material';
 /**
  * NgModule that includes all Material modules.
  */
 const MATERIAL_MODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
   MatDialogModule,
-  MatDialogRef
+  MatFormFieldModule,
+  MatInputModule,
+  MatTooltipModule,
 ];
 
 @NgModule({
