a46d8a92a77afc16b72c88119ed69b99a7293a22
Dylan Hanger <dylan.hanger@gmail.com>
2019-04-03 07:27:44 +0200

    Generated Course Details component

--- src/app/course-details/course-details.component.ts
+++ src/app/course-details/course-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseDetailsComponent implements OnInit {
 
-  constructor() { }
+  course$: Observable<any>;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.course$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        params.get('code')
+      ));
+    this.course$.subscribe(response => console.log(response));
   }
 
 }
